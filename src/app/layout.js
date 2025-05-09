5fb4a46fa730c2248ab1a9f2946893786d5ff9ee
Maqui2404 <marfimaqui24@gmail.com>
2025-05-08 22:05:54 -0500

    Compl 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,15 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Papa Francisco - Legado 2013-2025',
   description: 'Sitio de homenaje al legado del Papa Francisco, primer Papa latinoamericano, jesuita, y profundamente comprometido con la justicia, la ecología y la misericordia.',
+  keywords: 'Papa Francisco, Jorge Mario Bergoglio, Iglesia Católica, Vaticano, obras del Papa, biografía Papa',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Papa Francisco - Sitio informativo',
+    description: 'Explora el legado espiritual y social del Papa Francisco.',
+    url: 'https://papa-francisco.vercel.app',
+    siteName: 'Papa Francisco',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
